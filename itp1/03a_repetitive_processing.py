caa31da9b7189ae4191b93251e7d3bfe39de7d81
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-02-02 08:15:41 +0000

    Finish 3-a: Repetitive Processing

---
+++ itp1/03a_repetitive_processing.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+
+def main():
+    for i in range(1000):
+        print("Hello World")
+
+if __name__ == '__main__':
+    main()
+
+
+# NOTE AOJ question URL (Repetitive Processing - Print Many Hello World):
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_A
