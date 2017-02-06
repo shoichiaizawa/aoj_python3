fb3525f6e21a803ac57906c71b45169b5e1cb4c8
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-02-06 04:39:30 +0000

    Finish 5-a: Print a Rectangle

---
+++ itp1/05a_print_a_rectangle.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python3
+
+import sys
+
+
+def string_to_list_spliter():
+    h, w = [int(i) for i in sys.stdin.readline().split()]
+    return h, w
+
+
+def generate_ractangle(h, w):
+    rect = ''
+    for i in range(h):
+        for j in range(w):
+            rect += '#'
+        rect += '\n'
+    return rect
+
+
+def main():
+    while True:
+        h, w = string_to_list_spliter()
+        if h == 0 and w == 0:
+            break
+        print(generate_ractangle(h, w))
+
+
+if __name__ == '__main__':
+    main()
+
+
+# NOTE AOJ question URL (Structured Program I - Print a Rectangle):
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_5_A
