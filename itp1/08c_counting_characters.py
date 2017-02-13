77a63191224c5e6d625e727cad503c3352aa5214
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-02-13 06:06:50 +0000

    Finish 8-c: Counting Characters

---
+++ itp1/08c_counting_characters.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3
+
+import sys
+
+
+def main():
+    text = str()
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+
+    for line in sys.stdin:
+        text += line.lower()
+
+    for alphabet in alphabets:
+        print('%s : %d' % (alphabet, text.count(alphabet)))
+
+
+if __name__ == '__main__':
+    main()
+
+
+# NOTE: AOJ question URL (Character - Counting Characters):
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_8_C
