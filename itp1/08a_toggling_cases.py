6847338ba8b8cbcc4cfe7d273c9361824461958a
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-02-13 02:36:32 +0000

    Finish 8-a: Toggling Cases

---
+++ itp1/08a_toggling_cases.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+
+
+def main():
+    print(input().swapcase())
+
+
+if __name__ == '__main__':
+    main()
+
+
+# NOTE: AOJ question URL (Character - Toggling Cases):
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_8_A
+
+# NOTE: Inspiration resources:
+# - Python Official Documentations:
+#   https://docs.python.org/3.6/library/stdtypes.html#str.swapcase
